66656b4d9a9cc579816a2d48765e01f6ad583d8e
John Doeexit <2002eojin@gmail.com>
2023-07-07 21:16:02 +0900

    feat: Auth 요구사항 3까지 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,101 @@
+import { useCallback, useEffect, useState } from 'react';
+import API from '../api';
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access_token');
+
+    const checkLoggedIn = async () => {
+      let isLoggedIn = false;
+
+      if (accessToken) {
+        const res = await API.get('auth/profile', {
+          headers: { Authorization: 'Bearer ' + accessToken },
+        });
+
+        if (res.status !== 401) {
+          isLoggedIn = true;
+        }
+      }
+
+      if (isLoggedIn) {
+        alert('이미로그인되어있음');
+        navigate('/main');
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
+  const onChangeEmail = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(e.target.value);
+    },
+    [],
+  );
+
+  const onChangePassword = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+    },
+    [],
+  );
+
+  const login = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const login = async () => {
+        try {
+          const json = await API.post('auth/login', {
+            body: JSON.stringify({ email, password }),
+            headers: {
+              'Content-Type': `application/json`,
+            },
+          }).json();
+
+          // any는 임시방편
+          const { access_token, refresh_token } = json as any;
+
+          localStorage.setItem('access_token', access_token);
+          localStorage.setItem('refresh_token', refresh_token);
+
+          console.log('Login Complete!');
+
+          navigate('/main');
+        } catch (error) {
+          console.log(error);
+
+          console.log('Login Failed!');
+        }
+      };
+
+      login();
+    },
+    [email, password],
+  );
+
   return (
     <>
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form onSubmit={login}>
+        <input
+          value={email}
+          onChange={onChangeEmail}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={password}
+          onChange={onChangePassword}
+          type="password"
+          placeholder="password"
+        />
         <button type="submit" />
       </form>
     </>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,48 @@
 // import Category from '../components/Category';
 
+import { useCallback, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import API from '../api';
+
 const Login = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access_token');
+
+    const checkLoggedIn = async () => {
+      let isLoggedIn = false;
+
+      if (accessToken) {
+        const res = await API.get('auth/profile', {
+          headers: { Authorization: 'Bearer ' + accessToken },
+        });
+
+        if (res.status !== 401) {
+          isLoggedIn = true;
+        }
+      }
+
+      if (!isLoggedIn) {
+        alert('로그인안됨');
+        navigate('/');
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
+  const logout = useCallback(() => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    navigate('/');
+  }, []);
+
   return (
     <>
-      <button type="button">logout</button>
+      <button type="button" onClick={logout}>
+        logout
+      </button>
     </>
   );
 };
