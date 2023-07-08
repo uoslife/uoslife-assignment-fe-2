6e4d162f042e34697782b7d1f3a3850a439e4d9c
youngchan Kim <aacz12035@gmail.com>
2023-07-08 13:33:45 +0900

    feat: 메인 페이지 제작

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,48 @@
 // import Category from '../components/Category';
 
-const Login = () => {
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const LogOutButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 30px;
+  border: 1px black solid;
+  background-color: gray;
+  color: white;
+`;
+
+const Main = () => {
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+
+    if (!localStorage.getItem('access_token')) {
+      navigate('/login');
+      alert('로그아웃에 성공하셨습니다.');
+    }
+  };
+
   return (
-    <>
-      <button type="button">logout</button>
-    </>
+    <Container>
+      <div>로그인에 성공하셨습니다.</div>
+      <LogOutButton onClick={handleLogOut}>logout</LogOutButton>
+    </Container>
   );
 };
 
-export default Login;
+export default Main;
