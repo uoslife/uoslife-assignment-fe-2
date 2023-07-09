0aa137549f792e7dcf25a45778cb4f85e0953b29
Song Chaehee <sch003322@gmail.com>
2023-07-10 00:57:42 +0900

    feat: 요구사항1, 요구사항3 구현 완료

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,58 @@
+import React, { useState, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import API from '../api';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailChange = (event: any) => {
+    setEmail(event.target.value);
+  };
+  const passwordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const login = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const login = async () => {
+        try {
+          const response = await API.post('auth/login', {
+            body: JSON.stringify({ email, password }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          const { access_token, refresh_token } =
+            (await response.json()) as any;
+
+          localStorage.setItem('access_token', access_token);
+          localStorage.setItem('refresh_token', refresh_token);
+
+          navigate('/main');
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      login();
+    },
+    [email, password],
+  );
+
   return (
     <>
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button type="submit" />
+      <form onSubmit={login}>
+        <input type="email" placeholder="email" onChange={emailChange} />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={passwordChange}
+        />
+        <button type="submit">Login</button>
       </form>
     </>
   );
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,17 @@
 // import Category from '../components/Category';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    localStorage.clear();
+    navigate('/');
+  };
   return (
     <>
-      <button type="button">logout</button>
+      <button type="button" onClick={handleClick}>
+        logout
+      </button>
     </>
   );
 };
